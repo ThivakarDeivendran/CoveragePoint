6b788d5d4f5cae0f8fe3f1f496ed3c2704e9c0eb
ThivakarDeivendran <dthivakar1@gmail.com>
2024-06-17 11:34:35 +0530

    Allure Report configurations

--- src/test/java/testRunnerPackage/TestRunnerFile.java
+++ src/test/java/testRunnerPackage/TestRunnerFile.java
@@ -1,20 +1,19 @@
 package testRunnerPackage;
 
 import org.junit.runner.RunWith;
+import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
 
-@RunWith(Cucumber.class)
 @CucumberOptions(
 	    features = "src/test/java/featurePackage/", glue = {"stepdefinitions"}, 
 	    plugin = {
 	        "pretty", 
 	        "html:target/cucumber-html-report",
 	        "json:target/cucumber.json",
-	        "junit:target/cucumber.xml","io.qameta.allure.cuucmber7jvm.AllureCucumber7Jvm" },
+	        "junit:target/cucumber.xml","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm" },
 	    monochrome = true, dryRun = false)
-public class TestRunnerFile {
+public class TestRunnerFile extends AbstractTestNGCucumberTests{
 
 }
 
